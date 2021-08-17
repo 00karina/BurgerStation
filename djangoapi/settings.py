ebcca73ed41251cd6b96cd3e42527c2634e2a30e
karina shrestha <karinasrt00@gmail.com>
2021-08-17 12:21:05 +0545

    Hosting

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nxkdp9n$7stuq17^+%qx*teu2b&t1f!kv=%$4_6xnpd@k!^+z@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['burgerstation.herokuapp.com','127.0.0.1']
 
 
 # Application definition
