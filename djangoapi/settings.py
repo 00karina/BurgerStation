83fa9d8f706748b6414f99c8297c25202b975bc2
karina shrestha <karinasrt00@gmail.com>
2021-08-29 14:46:40 +0545

    commit

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,9 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nxkdp9n$7stuq17^+%qx*teu2b&t1f!kv=%$4_6xnpd@k!^+z@'
-
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['burgerstation.herokuapp.com','127.0.0.1']
 
